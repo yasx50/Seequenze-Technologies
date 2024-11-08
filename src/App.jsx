076e1dbcf64754659e00540dcb05d6184dfe3123
yasx50 <yasxcode@gmail.com>
2024-11-08 23:33:10 +0530

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -14,23 +14,23 @@ function App() {
       <div className="min-h-screen w-full overflow-x-hidden bg-black">
         <Header />
         
-        <div className="flex flex-row space-x-4 overflow-x-auto">
+        <div className="flex flex-col sm:flex-row sm:space-x-4 overflow-x-auto p-4">
           {/* Container for tasks */}
-          <div className="flex flex-col space-y-4">
+          <div className="flex flex-col space-y-4 sm:w-1/3 lg:w-1/4">
             <AddTaskForm />
             <CompletedTasks />
             <ExpiredTasks />
           </div>
           
           {/* Task Details and Task List */}
-          <div className="flex flex-col space-y-4">
+          <div className="flex flex-col space-y-4 sm:w-2/3 lg:w-3/4">
             <TaskDetail />
             <TaskList />
           </div>
         </div>
-
       </div>
       
+      <Footer />
     </>
   );
 }
