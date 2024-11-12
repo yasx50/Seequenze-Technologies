376b303ecde500f8ccc5d61f345b924adac22e0b
yasx50 <yasxcode@gmail.com>
2024-11-12 23:04:08 +0530

    old

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use('/api/auth', auth);
 app.use('/user',display)
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
 
